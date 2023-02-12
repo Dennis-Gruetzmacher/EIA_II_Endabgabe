6b648fa3842c049724bae05067b1fccee717c436
Dennis-Gruetzmacher <101892643+Dennis-Gruetzmacher@users.noreply.github.com>
2023-02-12 19:42:27 +0100

    CHA final

---
+++ EIAII_Endabgabe/script/Pattern.ts
@@ -0,0 +1,18 @@
+namespace CustomFirework 
+{
+    export enum Pattern 
+    {
+        circle = "circle",
+        star = "star",
+        cross = "cross"
+    }
+
+    export let patterns: { [key: string]: Pattern } = 
+    {
+        "circle": Pattern.circle,
+        "star": Pattern.star,
+        "cross": Pattern.cross
+
+    };
+
+}
